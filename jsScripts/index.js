e5510082daf43bde0204e3912a4767ffab2dbd13
admamayo <admamayo@gmail.com>
2024-01-13 13:46:57 +1100

    alpha build title typewriter

--- jsScripts/index.js
+++ jsScripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    typeWriter("#typewriter1", "Hi! I'm Adrian.", 0);
+    typeWriter("#typewriter1", "Hi! I'm Adrian.", 80);
 	setTimeout(function() {
 		typeWriter("#typewriter2", "Welcome to my corner of the internet.", 40);
 	}, 1500);
