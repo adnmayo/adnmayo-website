adbd42748857dc7a53be65a9a4a85d3d2dc2b2ab
admamayo <admamayo@gmail.com>
2024-02-20 21:07:08 +1100

    navbar functionality

--- jsScripts/index.js
+++ jsScripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    typeWriter("#typewriter1", "Hi! I'm Adrian.", 80);
+    typeWriter("#typewriter1", "Hi! I'm Adrian.", 70);
 	setTimeout(function() {
 		typeWriter("#typewriter2", "Welcome to my corner of the internet.", 40);
 	}, 1500);
