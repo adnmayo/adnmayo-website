d471c72b89a054f54146ff412b44a38cb510a4e9
admamayo <admamayo@gmail.com>
2024-02-20 22:39:48 +1100

    homepage remodel

--- jsScripts/index.js
+++ jsScripts/index.js
@@ -1,11 +1,14 @@
 $(document).ready(() => {
-    typeWriter("#typewriter1", "Hi! I'm Adrian.", 70);
+    typeWriter("#typewriter1", "Hi! I'm Adrian.", 60);
 	setTimeout(function() {
 		typeWriter("#typewriter2", "Welcome to my corner of the internet.", 40);
-	}, 1500);
+	}, 1600);
     setTimeout(function() {
 		typeWriter("#typewriter3", "Software Engineering @ UTS", 40);
 	}, 3500);
+	setTimeout(function() {
+		typeWriter("#typewriter4", "Roam around using the navigation menu to the left", 30)
+	}, 5000);
 });
 
 
