d84be46dcaf36b311f6f116b746a83320055b4d9
admamayo <admamayo@gmail.com>
2024-02-23 00:28:34 +1100

    Roll-out of Spanish site

---
+++ jsScripts/spanishindex.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    typeWriter("#typewriter1", "Hola! Soy Adrian.", 60);
+	setTimeout(function() {
+		typeWriter("#typewriter2", "Bienvenidos a mi rincon del Internet.", 40);
+	}, 1600);
+    setTimeout(function() {
+		typeWriter("#typewriter3", "Ingenieria de Software @ UTS", 40);
+	}, 3500);
+	setTimeout(function() {
+		typeWriter("#typewriter4", "Guiate usando el menu a la izquierda", 30)
+	}, 5000);
+});
+
+
